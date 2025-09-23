2931d9a32de0ef84422eabd02931b3d22f4dadca
mrdsx <andre0.castlegames@gmail.com>
2025-09-23 22:29:08 +0300

    feat(frontend/chat): add chat session name to ChatHeader

--- frontend/src/features/chat/components/ChatHeader.tsx
+++ frontend/src/features/chat/components/ChatHeader.tsx
@@ -1,20 +1,34 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import { ToggleThemeButton } from "@/features/theme";
 import { cn } from "@/lib";
+import { useQuery } from "@tanstack/react-query";
 import { useChatContext } from "../ChatContextProvider";
+import { getChatSession } from "../services";
 import { getChatStatusStyles } from "../utils";
 import { ShareSessionDialog } from "./ShareSessionDialog";
 
 function ChatHeader() {
-  const { chatStatus } = useChatContext();
+  const { chatStatus, sessionUUID } = useChatContext();
   const chatStatusStyles = getChatStatusStyles(chatStatus);
 
+  const { data, isPending } = useQuery({
+    queryKey: ["get_chat_session"],
+    queryFn: () => getChatSession(sessionUUID as string),
+  });
+
   return (
-    <div className="flex justify-between px-4 pb-4">
-      <span className="flex items-center gap-2">
-        <div className={cn("size-3 rounded-full", chatStatusStyles)}></div>
-        {chatStatus}
-      </span>
+    <header className="flex justify-between px-4">
+      <div className="flex items-center gap-4">
+        <span className="flex items-center gap-2">
+          <div className={cn("size-3 rounded-full", chatStatusStyles)}></div>
+          {chatStatus}
+        </span>
+        {!isPending && chatStatus === "Online" && (
+          <span className="hidden truncate sm:block sm:w-[40ch] md:w-[50ch] lg:w-[80ch]">
+            {data?.session_name}
+          </span>
+        )}
+      </div>
 
       <div className="flex items-center gap-2">
         {chatStatus === "Online" ? (
@@ -29,7 +43,7 @@ function ChatHeader() {
           </>
         )}
       </div>
-    </div>
+    </header>
   );
 }
 
